2ceebaefeebd6bff22646fc895f11b89f336721f
Olivier Bondu <olivier.bondu@gmail.com>
2015-09-01 11:31:48 +0200

    Update of eID tags

--- python/ElectronsProducer.py
+++ python/ElectronsProducer.py
@@ -9,13 +9,17 @@ default_configuration = cms.PSet(
             ea_R03 = cms.untracked.FileInPath('RecoEgamma/ElectronIdentification/data/PHYS14/effAreaElectrons_cone03_pfNeuHadronsAndPhotons.txt'),
             ea_R04 = cms.untracked.FileInPath('cp3_llbb/Framework/data/effAreaElectrons_cone04_pfNeuHadronsAndPhotons.txt'),
             ids = cms.untracked.VInputTag(
-                'egmGsfElectronIDs:mvaEleID-PHYS14-PU20bx25-nonTrig-V1-wp80',
-                'egmGsfElectronIDs:mvaEleID-PHYS14-PU20bx25-nonTrig-V1-wp90',
-                'egmGsfElectronIDs:cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-loose',
-                'egmGsfElectronIDs:cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-medium',
-                'egmGsfElectronIDs:cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-tight',
-                'egmGsfElectronIDs:cutBasedElectronID-PHYS14-PU20bx25-V2-standalone-veto',
-                'egmGsfElectronIDs:heepElectronID-HEEPV51'
+                'egmGsfElectronIDs:mvaEleID-Spring15-25ns-nonTrig-V1-wp80',
+                'egmGsfElectronIDs:mvaEleID-Spring15-25ns-nonTrig-V1-wp90',
+                'egmGsfElectronIDs:cutBasedElectronID-Spring15-50ns-V1-standalone-veto',
+                'egmGsfElectronIDs:cutBasedElectronID-Spring15-50ns-V1-standalone-loose',
+                'egmGsfElectronIDs:cutBasedElectronID-Spring15-50ns-V1-standalone-medium',
+                'egmGsfElectronIDs:cutBasedElectronID-Spring15-50ns-V1-standalone-tight',
+                'egmGsfElectronIDs:cutBasedElectronID-Spring15-25ns-V1-standalone-veto',
+                'egmGsfElectronIDs:cutBasedElectronID-Spring15-25ns-V1-standalone-loose',
+                'egmGsfElectronIDs:cutBasedElectronID-Spring15-25ns-V1-standalone-medium',
+                'egmGsfElectronIDs:cutBasedElectronID-Spring15-25ns-V1-standalone-tight',
+                'egmGsfElectronIDs:heepElectronID-HEEPV60'
                 ),
             scale_factors = cms.untracked.PSet(
                 id_veto = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Electron_sfVETO_id.json'),
