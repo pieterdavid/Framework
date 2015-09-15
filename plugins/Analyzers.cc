4fd63d228a12a57bb96fd606a5af90c9b4bfb1dd
Sébastien <sebastien.wertz@uclouvain.be>
2015-09-15 15:45:49 +0200

    Added BTagsAnalyzer

--- plugins/Analyzers.cc
+++ plugins/Analyzers.cc
@@ -1,7 +1,9 @@
 #include <FWCore/PluginManager/interface/PluginFactory.h>
 
 #include <cp3_llbb/Framework/interface/DileptonAnalyzer.h>
+#include <cp3_llbb/Framework/interface/BTagsAnalyzer.h>
 #include <cp3_llbb/Framework/interface/TestAnalyzer.h>
 
 DEFINE_EDM_PLUGIN(ExTreeMakerAnalyzerFactory, DileptonAnalyzer, "dilepton_analyzer");
+DEFINE_EDM_PLUGIN(ExTreeMakerAnalyzerFactory, BTagsAnalyzer, "btags_analyzer");
 DEFINE_EDM_PLUGIN(ExTreeMakerAnalyzerFactory, TestAnalyzer, "test_analyzer");
---
+++ src/BTagsAnalyzer.cc
@@ -0,0 +1,16 @@
+#include <cp3_llbb/Framework/interface/BTagsAnalyzer.h>
+
+#include <cp3_llbb/Framework/interface/JetsProducer.h>
+
+void BTagsAnalyzer::analyze(const edm::Event& event, const edm::EventSetup&, const ProducersManager& producers, const CategoryManager& categories) {
+    
+    const JetsProducer& jets = producers.get<JetsProducer>("jets");
+
+    for(unsigned int ijet = 0; ijet < jets.p4.size(); ijet++) {
+        if(jets.p4[ijet].Pt() > m_pt_cut && abs(jets.p4[ijet].Eta()) < m_eta_cut && jets.getBTagDiscriminant(ijet, m_discr_name) > m_discr_cut) {
+            indices.push_back(ijet);
+        }
+    }
+
+}
+
