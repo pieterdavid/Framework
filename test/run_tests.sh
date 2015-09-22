0b85fab59cfc2de28d34a9d0e099bae473228b12
Sébastien Brochet <sebastien.brochet@cern.ch>
2015-09-22 10:09:04 +0200

    Add data unit tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,3 +5,6 @@ function die { echo $1: status $2 ;  exit $2; }
 
 F1=${LOCAL_TEST_DIR}/unit_tests_mc.py
 (cmsRun $F1 ) || die "Failure using $F1" $?
+
+F2=${LOCAL_TEST_DIR}/unit_tests_data.py
+(cmsRun $F2 ) || die "Failure using $F2" $?
