f55f525814fc987aacb1a5f3e4af098ad90894d1
Sébastien Brochet <sebastien.brochet@cern.ch>
2015-09-23 14:40:48 +0200

    Filter HLT path based on XML file and store HLT objects

--- interface/HLTProducer.h
+++ interface/HLTProducer.h
@@ -2,17 +2,24 @@
 #define HLT_PRODUCER
 
 #include <cp3_llbb/Framework/interface/CandidatesProducer.h>
+#include <cp3_llbb/Framework/interface/HLTService.h>
 
 #include <FWCore/Common/interface/TriggerNames.h>
 #include <DataFormats/Common/interface/TriggerResults.h>
 #include <DataFormats/PatCandidates/interface/PackedTriggerPrescales.h>
+#include <DataFormats/PatCandidates/interface/TriggerObjectStandAlone.h>
 
 class HLTProducer: public Framework::Producer {
     public:
         HLTProducer(const std::string& name, const ROOT::TreeGroup& tree, const edm::ParameterSet& config):
             Producer(name, tree, config)
         {
-
+            if (config.exists("triggers")) {
+                m_hlt_service.reset(new HLTService(config.getUntrackedParameter<edm::FileInPath>("triggers").fullPath()));
+                std::cout << "HLT configuration: " << std::endl;
+                m_hlt_service->print();
+                std::cout << std::endl;
+            }
         }
 
         virtual ~HLTProducer() {}
@@ -20,6 +27,7 @@ class HLTProducer: public Framework::Producer {
         virtual void doConsumes(const edm::ParameterSet& config, edm::ConsumesCollector&& collector) override {
             m_hlt_token = collector.consumes<edm::TriggerResults>(config.getUntrackedParameter<edm::InputTag>("hlt", edm::InputTag("TriggerResults", "", "HLT")));
             m_prescales_token = collector.consumes<pat::PackedTriggerPrescales>(config.getUntrackedParameter<edm::InputTag>("prescales", edm::InputTag("patTrigger")));
+            m_trigger_objects_token = collector.consumes<pat::TriggerObjectStandAloneCollection>(config.getUntrackedParameter<edm::InputTag>("objects", edm::InputTag("selectedPatTrigger")));
         }
 
         virtual void produce(edm::Event& event, const edm::EventSetup& eventSetup) override;
@@ -29,11 +37,19 @@ class HLTProducer: public Framework::Producer {
         // Tokens
         edm::EDGetTokenT<edm::TriggerResults> m_hlt_token;
         edm::EDGetTokenT<pat::PackedTriggerPrescales> m_prescales_token;
+        edm::EDGetTokenT<pat::TriggerObjectStandAloneCollection> m_trigger_objects_token;
+
+        // Service
+        std::shared_ptr<HLTService> m_hlt_service;
 
     public:
         // Tree members
         std::vector<std::string>& paths = tree["paths"].write<std::vector<std::string>>();
         std::vector<uint16_t>& prescales = tree["prescales"].write<std::vector<uint16_t>>();
+
+        BRANCH(object_paths, std::vector<std::vector<std::string>>);
+        BRANCH(object_p4, std::vector<LorentzVector>);
+        BRANCH(object_pdg_id, std::vector<int>);
 };
 
 #endif
