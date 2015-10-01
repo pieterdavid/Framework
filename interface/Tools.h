096458d7176fb54b10fed7685db0de51f24a48b6
Sébastien Brochet <sebastien.brochet@cern.ch>
2015-10-01 10:57:22 +0200

    Store jetid for jets and fat jets

---
+++ interface/Tools.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace pat {
+    class Jet;
+}
+
+namespace Tools {
+    namespace Jets {
+
+        // Jet id: https://twiki.cern.ch/twiki/bin/view/CMS/JetID#Recommendations_for_13_TeV_data
+#define DECLARE_QUANTITIES(jet) \
+        float eta = jet.eta(); \
+        float NHF = jet.neutralHadronEnergyFraction(); \
+        float NEMF = jet.neutralEmEnergyFraction(); \
+        float CHF = jet.chargedHadronEnergyFraction(); \
+        float MUF = jet.muonEnergyFraction(); \
+        float CEMF = jet.chargedEmEnergyFraction(); \
+        size_t NumConst = jet.chargedMultiplicity() + jet.neutralMultiplicity(); \
+        size_t NumNeutralParticles = jet.neutralMultiplicity(); \
+        size_t CHM = jet.chargedMultiplicity();
+
+        bool passLooseId(const pat::Jet& jet);
+        bool passTightId(const pat::Jet& jet);
+        bool passTightLeptonVetoId(const pat::Jet& jet);
+    };
+};
