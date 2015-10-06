8408306fa933539750622e965101b9b8b93f23ba
Sébastien Brochet <sebastien.brochet@cern.ch>
2015-10-06 14:49:11 +0200

    Add support for JEC databases

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -8,3 +8,7 @@ F1=${LOCAL_TEST_DIR}/unit_tests_mc.py
 
 F2=${LOCAL_TEST_DIR}/unit_tests_data.py
 (cmsRun $F2 ) || die "Failure using $F2" $?
+
+wget https://github.com/cms-jet/JECDatabase/raw/master/SQLiteFiles/Summer15_25nsV5_MC.db
+F3=${LOCAL_TEST_DIR}/unit_tests_mc_with_db.py
+(cmsRun $F3 ) || die "Failure using $F3" $?
