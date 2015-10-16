4d196d6caf75c36511f7fda0480bd7c9b65f10ff
Sébastien Brochet <sebastien.brochet@cern.ch>
2015-10-16 09:05:46 +0200

    Make pruned particles branches transient

--- interface/Types.h
+++ interface/Types.h
@@ -3,5 +3,6 @@
 #include <Math/Vector4D.h>
 
 #define BRANCH(NAME, ...) __VA_ARGS__& NAME = tree[#NAME].write<__VA_ARGS__>()
+#define TRANSIENT_BRANCH(NAME, ...) __VA_ARGS__& NAME = tree[#NAME].transient_write<__VA_ARGS__>()
 
 typedef ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<float>> LorentzVector;
