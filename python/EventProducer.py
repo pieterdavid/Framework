c2dd00910c66716242487d93a03c861a41ec3e00
Sébastien Brochet <sebastien.brochet@cern.ch>
2016-01-15 11:55:45 +0100

    Add PU systematics

--- python/EventProducer.py
+++ python/EventProducer.py
@@ -8,7 +8,13 @@ default_configuration = cms.PSet(
             pu_summary = cms.untracked.InputTag('slimmedAddPileupInfo'),
             compute_pu_weights = cms.untracked.bool(True),
             pu_reweighter = cms.PSet(
-                    data_pu_profile = cms.untracked.FileInPath('cp3_llbb/Framework/data/PUProfiles/pu_histogram_all_2015.root')
+                    data_pu_profile = cms.untracked.FileInPath('cp3_llbb/Framework/data/PUProfiles/pu_histogram_all_2015_69000minbias.root'),
+                ),
+            pu_reweighter_up = cms.PSet(
+                    data_pu_profile = cms.untracked.FileInPath('cp3_llbb/Framework/data/PUProfiles/pu_histogram_all_2015_72450minbias.root')
+                ),
+            pu_reweighter_down = cms.PSet(
+                    data_pu_profile = cms.untracked.FileInPath('cp3_llbb/Framework/data/PUProfiles/pu_histogram_all_2015_65550minbias.root')
                 )
             )
         )
--- src/EventProducer.cc
+++ src/EventProducer.cc
@@ -25,6 +25,8 @@ void EventProducer::produce(edm::Event& event_, const edm::EventSetup& eventSetu
 
             if (m_pu_reweighter.get())
                 pu_weight = m_pu_reweighter->getWeight(true_interactions);
+                pu_weight_up = m_pu_reweighter_up->getWeight(true_interactions);
+                pu_weight_down = m_pu_reweighter_down->getWeight(true_interactions);
         }
     }
 
