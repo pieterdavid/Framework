d94490013c3e4ff4a14ccf714ad0f3d1eae38c97
Sébastien Brochet <sebastien.brochet@cern.ch>
2016-02-02 22:35:51 +0100

    Validate Framework output during unit tests
    The tree produced by the framework is compared to a reference tree to
    ensure that nothing changed.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,10 +5,13 @@ function die { echo $1: status $2 ;  exit $2; }
 
 F1=${LOCAL_TEST_DIR}/unit_tests_mc.py
 (cmsRun $F1 ) || die "Failure using $F1" $?
+(cp3llbbTestFrameworkOutput ${LOCAL_TEST_DIR}/unit_tests_mc_ref.root output_mc.root) || die "Output file does not match reference file" $?
 
 F2=${LOCAL_TEST_DIR}/unit_tests_data.py
 (cmsRun $F2 ) || die "Failure using $F2" $?
+(cp3llbbTestFrameworkOutput ${LOCAL_TEST_DIR}/unit_tests_data_ref.root output_data.root) || die "Output file does not match reference file" $?
 
 wget https://github.com/cms-jet/JECDatabase/raw/master/SQLiteFiles/Summer15_25nsV5_MC.db
 F3=${LOCAL_TEST_DIR}/unit_tests_mc_with_db.py
 (cmsRun $F3 ) || die "Failure using $F3" $?
+(cp3llbbTestFrameworkOutput ${LOCAL_TEST_DIR}/unit_tests_mc_with_db_ref.root output_mc.root) || die "Output file does not match reference file" $?
