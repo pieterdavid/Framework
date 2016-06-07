f70aa3bbc97db0d399d2a1784a51ec509cb73375
Sébastien Brochet <sebastien.brochet@cern.ch>
2016-06-07 15:00:22 +0200

    Update configuration files

--- test/TestConfigurationData.py
+++ test/TestConfigurationData.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.Eras import eras
 from cp3_llbb.Framework import Framework
 
-framework = Framework.Framework(True, eras.Run2_25ns, globalTag='80X_dataRun2_Prompt_v8', processName='reRECO')
+framework = Framework.Framework(True, eras.Run2_25ns, globalTag='80X_dataRun2_Prompt_v8', processName='RECO')
 
 framework.addAnalyzer('dilepton', cms.PSet(
         type = cms.string('dilepton_analyzer'),
@@ -31,5 +31,7 @@ framework.doSystematics(['jec', 'jer'])
 process = framework.create()
 
 process.source.fileNames = cms.untracked.vstring(
-        '/store/relval/CMSSW_8_0_5/DoubleMuon/MINIAOD/80X_dataRun2_relval_v9_RelVal_doubMu2015D-v1/00000/60218FF9-A008-E611-87E3-0CC47A4D7662.root'
+        '/store/data/Run2016B/DoubleMuon/MINIAOD/PromptReco-v2/000/273/158/00000/A6AC80E5-121A-E611-A689-02163E01439E.root'
         )
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
