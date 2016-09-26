278d3168ce6a2d0fab467b8e44d856119d536719
Olivier Bondu <olivier.bondu@gmail.com>
2016-09-26 11:55:18 +0200

    Update to 80X muon SFs and adding utilities
    delete old stuff
    
    new SF and README update
    
    removing old muon files, renaming utilities
    
    adding new eff file merging utility,
    original source files as well as final framework files
    
    updating producer with new SF files
    
    Adding tracking SF

---
+++ data/ScaleFactors/sources/muons/README.md
@@ -0,0 +1,10 @@
+Files found here on September 26th 2016:
+ - https://twiki.cern.ch/twiki/bin/viewauth/CMS/MuonWorkInProgressAndPagResults
+
+HWW scale-factors:
+ - https://twiki.cern.ch/twiki/bin/view/CMS/HWW2016TriggerAndIdIsoScaleFactorsResults
+ - merged together with `mergeEfficiencyFiles.py`
+
+Tracking efficiency SF:
+ - https://twiki.cern.ch/twiki/bin/view/CMS/MuonReferenceEffsRun2#Tracking_efficiency_provided_by
+
--- python/MuonsProducer.py
+++ python/MuonsProducer.py
@@ -10,19 +10,21 @@ default_configuration = cms.PSet(
             ea_R03 = cms.untracked.FileInPath('cp3_llbb/Framework/data/effAreaMuons_cone03_pfNeuHadronsAndPhotons.txt'),
             ea_R04 = cms.untracked.FileInPath('cp3_llbb/Framework/data/effAreaMuons_cone04_pfNeuHadronsAndPhotons.txt'),
             scale_factors = cms.untracked.PSet(
-                id_soft  = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_SoftID_genTracks_id.json'),
+                tracking = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_tracking_ratio_eta_2016_9p2.json'),
                 id_loose  = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_LooseID_genTracks_id.json'),
                 id_medium = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_MediumID_genTracks_id.json'),
                 id_tight  = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_TightIDandIPCut_genTracks_id.json'),
 
-                iso_loose_id_loose = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_LooseRelIso_LooseID_iso.json'),
-                iso_loose_id_medium = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_LooseRelIso_MediumID_iso.json'),
                 iso_loose_id_tight = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_LooseRelIso_TightID_iso.json'),
-                iso_tight_id_medium = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_TightRelIso_MediumID_iso.json'),
+                iso_loosetk_id_tight = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_LooseRelTkIso_TightID_iso.json'),
                 iso_tight_id_tight = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_TightRelIso_TightID_iso.json'),
 
-                id_hww = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_MediumID_Data_MC_25ns_PTvsETA_HWW_76.json'),
-                iso_tight_id_hww = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_ISOTight_Data_MC_25ns_PTvsETA_HWW.json'),
+                id_tight_hww_period1 = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_data_mc_TightID_Run2016_Run271036to275783_PTvsETA_HWW.json'),
+                id_tight_hww_period2 = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_data_mc_TightID_Run2016_Run275784to276500_PTvsETA_HWW.json'),
+                id_tight_hww_period3 = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_data_mc_TightID_Run2016_Run276501to276811_PTvsETA_HWW.json'),
+                iso_tight_hww_period1 = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_data_mc_ISOTight_Run2016_Run271036to275783_PTvsETA_HWW.json'),
+                iso_tight_hww_period2 = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_data_mc_ISOTight_Run2016_Run275784to276500_PTvsETA_HWW.json'),
+                iso_tight_hww_period3 = cms.untracked.FileInPath('cp3_llbb/Framework/data/ScaleFactors/Muon_data_mc_ISOTight_Run2016_Run276501to276811_PTvsETA_HWW.json'),
                 )
             )
         )
