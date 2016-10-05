bd6655a0b00c8857543d60b7528f22b5ee1cd186
Olivier Bondu <olivier.bondu@gmail.com>
2016-10-05 17:59:38 +0200

    updating PF jet ID

--- src/Tools.cc
+++ src/Tools.cc
@@ -12,27 +12,31 @@ namespace Tools {
         bool passLooseId(const pat::Jet& jet) {
             DECLARE_QUANTITIES(jet);
 
-            if (fabs(eta) <= 3.0) {
-                return (NHF < 0.99 && NEMF < 0.99 && NumConst > 1) && ((fabs(eta) <= 2.4 && CHF > 0 && CHM > 0 && CEMF < 0.99) || fabs(eta) > 2.4);
+            if (fabs(eta) <= 2.7) {
+                return (NHF < 0.99 && NEMF < 0.99 && NumConst > 1) && ((fabs(eta) <= 2.4 && CHF > 0 && CHM > 0 && CEMF < 0.99) || fabs(eta) > 2.4) && fabs(eta) <= 2.7;
+            } else if (fabs(eta) > 2.7 && fabs(eta) <= 3.0) {
+                return (NEMF < 0.90 && NumNeutralParticles > 2 && fabs(eta) > 2.7 && fabs(eta) <= 3.0);
             } else {
-                return (NEMF < 0.90 && NumNeutralParticles > 10);
+                return (NEMF < 0.90 && NumNeutralParticles > 10 && fabs(eta)>3.0);
             }
         }
 
         bool passTightId(const pat::Jet& jet) {
             DECLARE_QUANTITIES(jet);
 
-            if (fabs(eta) <= 3.0) {
-                return (NHF < 0.90 && NEMF < 0.90 && NumConst > 1) && ((fabs(eta) <= 2.4 && CHF > 0 && CHM > 0 && CEMF < 0.99) || fabs(eta) > 2.4);
+            if (fabs(eta) <= 2.7) {
+                return (NHF < 0.90 && NEMF < 0.90 && NumConst > 1) && ((fabs(eta) <= 2.4 && CHF > 0 && CHM > 0 && CEMF < 0.99) || fabs(eta) > 2.4) && fabs(eta) <= 2.7;
+            } else if (fabs(eta) > 2.7 && fabs(eta) <= 3.0) {
+                return (NEMF < 0.90 && NumNeutralParticles > 2 && fabs(eta) > 2.7 && fabs(eta)<=3.0);
             } else {
-                return (NEMF < 0.90 && NumNeutralParticles > 10);
+                return (NEMF < 0.90 && NumNeutralParticles > 10 && fabs(eta) > 3.0);
             }
         }
 
         bool passTightLeptonVetoId(const pat::Jet& jet) {
             DECLARE_QUANTITIES(jet);
 
-            return (NHF < 0.90 && NEMF < 0.90 && NumConst > 1 && MUF < 0.8) && ((fabs(eta) <= 2.4 && CHF > 0 && CHM > 0 && CEMF < 0.90) || fabs(eta) > 2.4) && fabs(eta) <= 3.0;
+            return (NHF < 0.90 && NEMF < 0.90 && NumConst > 1 && MUF < 0.8) && ((fabs(eta) <= 2.4 && CHF > 0 && CHM > 0 && CEMF < 0.90) || fabs(eta) > 2.4) && fabs(eta) <= 2.7;
         }
 #pragma GCC diagnostic pop
     };
