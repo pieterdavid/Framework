1bd0a018d77536683d503a92e645bb0cdb433dd3
Olivier Bondu <olivier.bondu@gmail.com>
2017-09-21 12:15:49 +0200

    updating ref trees and location of test trees

--- test/download_dependencies.sh
+++ test/download_dependencies.sh
@@ -4,9 +4,9 @@
 
 # Input files
 
-if [ -d /afs/cern.ch/user/s/sbrochet/www/public/CP3 ]; then
-    ln -s /afs/cern.ch/user/s/sbrochet/www/public/CP3/*.root .
+if [ -d /afs/cern.ch/user/o/obondu/public/CP3 ]; then
+    ln -s /afs/cern.ch/user/o/obondu/public/CP3/*.root .
 else
-    [ -f DoubleMuon_Run2016B_PromptReco-v2_reduced.root ] || wget https://sbrochet.web.cern.ch/sbrochet/public/CP3/DoubleMuon_Run2016B_PromptReco-v2_reduced.root
-    [ -f TTTo2L2Nu_13TeV-powheg_RunIISpring16MiniAODv2_reduced.root ] || wget https://sbrochet.web.cern.ch/sbrochet/public/CP3/TTTo2L2Nu_13TeV-powheg_RunIISpring16MiniAODv2_reduced.root
+    [ -f DoubleMuon_Run2016B_PromptReco-v2_reduced.root ] || wget https://obondu.web.cern.ch/obondu/public/CP3/DoubleMuon_Run2016B_PromptReco-v2_reduced.root
+    [ -f TTTo2L2Nu_13TeV-powheg_RunIISpring16MiniAODv2_reduced.root ] || wget https://obondu.web.cern.ch/obondu/public/CP3/TTTo2L2Nu_13TeV-powheg_RunIISpring16MiniAODv2_reduced.root
 fi
