957ac7ef22c30669ed9d40095ae15e05d4368d54
Pieter David <pieter.david@cern.ch>
2017-09-27 12:22:13 +0200

    clone to tmp and read CMSSW.arch and CMSSW.release from there

--- setup_project_with_framework.sh
+++ setup_project_with_framework.sh
@@ -2,26 +2,17 @@
 
 startdir=$(pwd)
 
-cmssw_arch="slc6_amd64_gcc530" ## should be in sync with CMSSW.arch
-cmssw_version="CMSSW_8_0_30"   ## should be in sync with CMSSW.release
+tempdir=$(mktemp -d)
+fwkdir_tmp="${tempdir}/Framework"
+
+git clone -o upstream https://github.com/cp3-llbb/Framework "${fwkdir_tmp}"
+
+cmssw_arch=$(cat "${fwkdir_tmp}/CMSSW.arch")
+cmssw_version=$(cat "${fwkdir_tmp}/CMSSW.release")
 
 scramv1 -a "${cmssw_arch}" project CMSSW "${cmssw_version}"
 cd "${cmssw_version}"
 cmsenv
-
-git clone -o upstream https://github.com/cp3-llbb/Framework
-fwkdir_tmp="${CMSSW_BASE}/Framework"
-
-upstream_cmssw_arch=$(cat "Framework/CMSSW.arch")
-if [ "${upstream_cmssw_arch}" != "${cmssw_arch}" ]; then
-  echo "Platform mismatch with CMSSW.arch: ${upstream_cmssw_arch}, while set up with ${cmssw_arch}"
-  exit 1
-fi
-upstream_cmssw_version=$(cat "Framework/CMSSW.release")
-if [ "${upstream_cmssw_version}" != "${cmssw_version}" ]; then
-  echo "CMSSW version mismatch with CMSSW.release: ${upstream_cmssw_version}, while set up with ${cmssw_version}"
-  exit 1
-fi
 
 ## apply recipes
 cd "${CMSSW_BASE}/src"
@@ -30,10 +21,11 @@ cd "${CMSSW_BASE}/src"
 llbbdir="${CMSSW_BASE}/src/cp3_llbb"
 mkdir -p "${llbbdir}"
 mv "${fwkdir_tmp}" "${llbbdir}/"
+rmdir "${tempdir}"
 ## compile
-scram b -j4
+scram b -j7
 ## check out a few more
-. "${fwkpkg}/jenkins_postbuild.sh"
+. "${llbbdir}/Framework/jenkins_postbuild.sh"
 
 ## add git remotes
 cd "${llbbdir}/Framework"
