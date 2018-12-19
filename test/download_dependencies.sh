e3c7d845fed82a645bbf0fae8010f6c74d9f4d61
Pieter David <pieter.david@cern.ch>
2018-12-19 11:37:31 +0100

    Change test files directory

--- test/download_dependencies.sh
+++ test/download_dependencies.sh
@@ -3,10 +3,6 @@
 [ -f Spring16_25nsV1_MC.db ] || wget https://github.com/cms-jet/JECDatabase/raw/master/SQLiteFiles/Spring16_25nsV1_MC.db
 
 # Input files
-
-if [ -d /afs/cern.ch/user/o/obondu/public/CP3 ]; then
-    ln -s /afs/cern.ch/user/o/obondu/public/CP3/*.root .
-else
-    [ -f DoubleMuon_Run2016B_PromptReco-v2_reduced.root ] || wget https://obondu.web.cern.ch/obondu/public/CP3/DoubleMuon_Run2016B_PromptReco-v2_reduced.root
-    [ -f TTTo2L2Nu_13TeV-powheg_RunIISpring16MiniAODv2_reduced.root ] || wget https://obondu.web.cern.ch/obondu/public/CP3/TTTo2L2Nu_13TeV-powheg_RunIISpring16MiniAODv2_reduced.root
-fi
+testfileshome="http://cp3.irmp.ucl.ac.be/~pdavid/llbbframeworktest"
+[ -f DoubleMuon_Run2016B_PromptReco-v2_reduced.root ] || wget "${testfileshome}/DoubleMuon_Run2016B_PromptReco-v2_reduced.root"
+[ -f TTTo2L2Nu_13TeV-powheg_RunIISpring16MiniAODv2_reduced.root ] || wget "${testfileshome}/TTTo2L2Nu_13TeV-powheg_RunIISpring16MiniAODv2_reduced.root"
